7fae8735bec204e760285f0975ebbe691c1a5cbe
LarryMarv <hillarymarvince@gmail.com>
2023-09-08 11:59:50 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - This function prints the defined words
+ *
+ * Return: Always return zero if its a success
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,/n");
+	return (0);
+}
