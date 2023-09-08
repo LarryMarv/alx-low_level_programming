6da2b202939bca3d86cdedae1dc70db0288b4aa9
LarryMarv <hillarymarvince@gmail.com>
2023-09-08 11:45:03 -0700

    Hello Puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - this main function prints the defined words
+ * 
+ * Return:returns zero if success
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
