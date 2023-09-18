70282539387424f3f506b3a043d61a5b6f5d69c1
LarryMarv <hillarymarvince@gmail.com>
2023-09-18 16:59:08 +0300

    Task 4. Reverse Prints

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* print_rev - Functions that prints in reverse
+* @s: String to be printed in reverse
+* Return: Void
+*/
+
+void print_rev(char *s)
+{
+	int i;
+	int count = 0;
+
+	for (i = 0 ; s[i] != '\0' ; i++)
+	count++;
+		for (i = count - 1 ; i >= 0 ; i--)
+		_putchar(s[i]);
+	_putchar('\n');
+}
