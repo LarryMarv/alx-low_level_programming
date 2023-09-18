3c18fdf8164fcb6aec352b7e425108852592c452
LarryMarv <hillarymarvince@gmail.com>
2023-09-18 19:20:24 +0300

    Task 5. Reverse String

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,23 +1,22 @@
 #include "main.h"
 
 /**
-* rev_string - Reverses a string
-* @s: Parameter
-* Return: Void
-*/
+ * rev_string - Reverse a string
+ * @s: The string to be modified
+ * Return: void
+ */
 
 void rev_string(char *s)
 {
-int c = 0;
+	int len = 0, index = 0;
+	char tmp;
 
-	while (s[c] != '\0')
+	while (s[index++])
+	len++;
+	for (index = len - 1; index >= len / 2; index--)
 	{
-	c++;
+	tmp = s[index];
+	s[index] = s[len - index - 1];
+	s[len - index - 1] = tmp;
 	}
-
-	for (c -=  1; c >= 0; c--)
-	{
-	_putchar(s[c]);
-	}
-	_putchar('\n');
 }
