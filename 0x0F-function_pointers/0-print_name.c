1ee3a00b7649a9726c33c1418aee94087f3c587c
LarryMarv <hillarymarvince@gmail.com>
2023-10-11 22:51:38 +0300

    My name is Hill

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -5,6 +5,7 @@
 * print_name - Prints a name as it is
 * @name: Name to be printed
 * @f: Pointer to a function that prints a name
+* Return: name and nothing
 */
 
 void print_name(char *name, void (*f)(char *))
