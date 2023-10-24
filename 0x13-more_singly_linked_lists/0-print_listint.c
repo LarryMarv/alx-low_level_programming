b3ba78a4384096bf558076189b2d90f232e980b7
LarryMarv <hillarymarvince@gmail.com>
2023-10-24 12:43:42 +0300

    Task 1

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -7,13 +7,13 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	int num = 0;
+	size_t count = 0;
 
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
-		num++;
+		count++;
 		h = h->next;
 	}
-	return (num);
+	return (count);
 }
